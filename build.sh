9ab6bf8f6571a3b2ee81303364b9843f941a20a6
Jody Lognoul <lognoulj@gmail.com>
2014-05-03 11:08:12 +0700

    05-03-2014

---
+++ build.sh
@@ -0,0 +1,3 @@
+git add .
+NOW=$(date +"%m-%d-%Y")
+git commit -am $NOW
---
+++ public/js/app/event/index/viewProgressbar.js
@@ -0,0 +1,33 @@
+define(['backbone','handlebars', 'underscore','moment'], function(Backbone, Handlebars, _, moment) {
+
+	var viewProgressbar = Backbone.View.extend({
+		
+		// Class
+		className: "progress progress-striped active",
+		tagName: "div",
+
+		// Templates
+		template: _.template( $('.progressbar-script').html() ),
+
+		initialize: function(){
+			var val = 1;
+			var _this = this;
+
+			setInterval( function(){
+				_this.render( (val)%110 );
+				val++;
+			}, 10 );  // run
+		},
+		render: function(value) {
+			var params = {
+				value: value
+			};
+			this.$el.html( this.template( params ) );
+
+			return this;
+		}
+	});
+
+	return viewProgressbar;
+});
+
