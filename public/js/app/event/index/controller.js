6ea15c993545dab9912e5a6743b681b086ff3c07
Jody Lognoul <lognoulj@gmail.com>
2014-05-05 19:04:25 +0700

    05-05-2014

--- public/js/app/event/index/controller.js
+++ public/js/app/event/index/controller.js
@@ -1,4 +1,4 @@
-define(['underscore','collection','viewCollection'],function(_, EventsCollection, ViewEvents) {
+define(['underscore','collection','viewCollection','viewProgressbar','viewActiveFilterSorting'],function(_, EventsCollection, ViewEvents, ViewProgressbar, ViewActiveFilterSorting) {
 
 	var ControllerEventsIndex = function(options){
 		this.initialize(options);
@@ -6,6 +6,9 @@ define(['underscore','collection','viewCollection'],function(_, EventsCollection
 	};
 
 	_.extend(ControllerEventsIndex.prototype, {
+		options: {
+			defaultSortedBy: 'id'
+		},
 		initialize: function(options){
 			this.router				= options.router;
 			this.collection			= new EventsCollection();
@@ -13,24 +16,82 @@ define(['underscore','collection','viewCollection'],function(_, EventsCollection
 		listen: function(){
 			document.addEventListener('datetimepickerEvents',this.actionDatePicker.bind(this));
 		},
+		// ACTIONS
 		actionDatePicker: function(e){
 			this.router.navigate('filter/eventdate/' + e.detail, {trigger: true});
 		},
 		actionDefault: function(){
 			if (this.fetch(this.actionDefault)) {
-				var viewEvents = new ViewEvents({ collection: this.collection }, this.router);
-				$('.dest-bb-events').empty().append(viewEvents.el);
+				this.collection.changeSort(this.options.defaultSortedBy);
+				this.appendViewEvents(this.collection);
+				this.removeViewProgressbar(); // Appended in this.fetch()
 			}
 		},
-		fetch: function(callback){
+		actionSort: function(params){
+			if (this.fetch(this.actionSort, params)) {
+				this.collection.changeSort(params[0]);
+				this.appendViewEvents(this.collection);
+				this.appendViewActiveFilterSorting(params[0], 'sort' ,this.collection.length);
+				this.removeViewProgressbar(); // Appended in this.fetch()
+			}
+		},
+		actionFilter: function(params){
+			if (this.fetch(this.actionSort, params)) {
+				var filteredCollection;
+
+				// param[0] egale a 'by'  voir (public/js/app/event/index/router.js)
+				// param[1] egale a 'query' voir (public/js/app/event/index/router.js)
+
+				if( params[0] === 'eventdate' ){	// search by event_date
+					var event_date = moment(params[1]).format('YYYY-MM-DD');
+
+					filteredCollection = new EventsCollection(this.collection.where({ 'event_date': event_date }));
+					this.appendViewActiveFilterSorting(event_date, 'filter.eventdate', filteredCollection.length);
+
+				}else if(by === 's'){				// search by string
+					filteredCollection = new EventsCollection(this.eventsCollection.filterByString(params[1]));
+					this.appendViewActiveFilterSorting(params[1], 'filter.s',filteredCollection.length);
+				}
+
+				this.appendViewEvents(filteredCollection);
+				this.removeViewProgressbar(); // Appended in this.fetch()
+			}
+		},
+		// APPEND METHODS
+		// Events
+		appendViewEvents: function(_collection){
+			var viewEvents = new ViewEvents({ collection: _collection }, this.router);
+			$('.dest-bb-events').empty().append(viewEvents.el);
+		},
+		// Progressbar
+		appendViewProgressbar: function(){
+			this.viewProgressbar = new ViewProgressbar();
+			$('.dest-active-filters').empty().append(this.viewProgressbar.el);
+		},
+		removeViewProgressbar: function(){
+			if(this.viewProgressbar)
+				this.viewProgressbar.remove();
+		},
+		// ActiveFilterSorting
+		appendViewActiveFilterSorting: function(by, type, qty){
+			this.viewActivesFilterSorting = new ViewActiveFilterSorting({ model : by }, this.router, type, qty);
+			$('.dest-active-filters').empty().append(this.viewActivesFilterSorting.el);
+		},
+		removeViewActiveFilterSorting: function(){
+			if(this.viewActivesFilterSorting)
+				this.viewActivesFilterSorting.remove();
+		},
+		// FETCH
+		fetch: function(callback, params){
+			this.appendViewProgressbar();
 			if (this.collection.length === 0) {
-				this.collection.fetch().then(callback.bind(this));
+				this.collection.fetch().then(callback.bind(this, params));
 				return false;
 			}
 			return true;
 		}
 	});
 
-	return ControllerEventsIndex;
-	});
+return ControllerEventsIndex;
+});
 
--- public/js/app/event/index/router.js
+++ public/js/app/event/index/router.js
@@ -1,88 +1,26 @@
 // /public/js/app/event/index/router.js
 
-define(['jquery', 'backbone','underscore','moment','collection','viewCollection','viewActiveFilterSorting','viewProgressbar','controller'], function($, Backbone, _, moment, EventsCollection, ViewEvents, ViewActiveFilterSorting, ViewProgressbar,ControllerEventsIndex){
+define(['jquery', 'backbone','underscore','moment','collection','viewCollection','viewActiveFilterSorting','controller'], function($, Backbone, _, moment, EventsCollection, ViewEvents, ViewActiveFilterSorting,ControllerEventsIndex){
 
 	var AppRouter = Backbone.Router.extend({
-		routes: {
-			''					:'defaultRoute',
-			'sort/:dir/:by'		:'sortRoute',
-			'filter/:by/:query'	:'filterRoute',
-		},
+
 		initialize: function(){
 			this.controller = new ControllerEventsIndex({router:this});
 			this.eventsCollection = new EventsCollection();
 		},
+		routes: {
+			''					: 'defaultRoute',
+			'sort/:dir/:by'		: 'sortRoute',
+			'filter/:by/:query'	: 'filterRoute',
+		},
 		defaultRoute: function(){
 			this.controller.actionDefault();
-			
-			// var _this = this;
-			// this.appendViewProgressbar();
-
-			// this.eventsCollection.fetch().then(function(){
-
-			// 	_this.removeViewActiveFilterSorting();
-			// 	_this.eventsCollection.changeSort('id');
-			// 	_this.appendViewEvents(_this.eventsCollection);
-			// 	_this.removeViewProgressbar();
-			// });
 		},
 		sortRoute: function(dir, by){
-			var _this = this;
-			this.appendViewProgressbar();
-			this.eventsCollection.fetch().then(function(){
-
-				_this.eventsCollection.changeSort(by);
-				_this.appendViewEvents(_this.eventsCollection);
-				_this.appendViewActiveFilterSorting(by, 'sort', _this.eventsCollection.length);
-				_this.removeViewProgressbar();
-			});
-
+			this.controller.actionSort([by, dir]);
 		},
 		filterRoute: function(by, query){
-			var filteredEventsCollection, _this = this;
-
-			this.appendViewProgressbar();
-			this.eventsCollection.fetch().then(function(){
-
-				if( by === 'eventdate' ){	// search by event_date
-					var event_date = moment(query).format('YYYY-MM-DD');
-
-					filteredEventsCollection = new EventsCollection(_this.eventsCollection.where({ 'event_date': event_date }));
-					_this.appendViewActiveFilterSorting(event_date, 'filter.eventdate',filteredEventsCollection.length);
-
-				}else if(by === 's'){		// search by string
-					filteredEventsCollection = new EventsCollection(_this.eventsCollection.filterByString(query));
-					_this.appendViewActiveFilterSorting(query, 'filter.s',filteredEventsCollection.length);
-				}
-				_this.appendViewEvents(filteredEventsCollection);
-				_this.removeViewProgressbar();
-			});
-
-		},
-		// View Collection
-		appendViewEvents: function(_collection){
-			var viewEvents = new ViewEvents({ collection: _collection }, this);
-			$('.dest-bb-events').empty().append(viewEvents.el);
-		},
-
-		// View ActiveFilterSorting
-		appendViewActiveFilterSorting: function(by, type, qty){
-			this.viewActivesFilterSorting = new ViewActiveFilterSorting({ model : by }, this, type, qty);
-			$('.dest-active-filters').empty().append(this.viewActivesFilterSorting.el);
-		},
-		removeViewActiveFilterSorting: function(){
-			if(this.viewActivesFilterSorting)
-				this.viewActivesFilterSorting.remove();
-		},
-
-		// View Progressbar
-		appendViewProgressbar: function(){
-			this.viewProgressbar = new ViewProgressbar();
-			$('.dest-active-filters').empty().append(this.viewProgressbar.el);
-		},
-		removeViewProgressbar: function(){
-			if(this.viewProgressbar)
-				this.viewProgressbar.remove();
+			this.controller.actionFilter([by, query]);
 		}
 	});
 return AppRouter;
